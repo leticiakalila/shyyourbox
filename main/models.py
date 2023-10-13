9af8b48bbbab0ae80b78fb538ce93f311358846e
leticiakalila <leticiakaliladarlene@gmail.com>
2023-10-13 11:11:03 +0700

    mengimplementasikan ajax

--- main/models.py
+++ main/models.py
@@ -7,5 +7,6 @@ class Product(models.Model):
     name = models.CharField(max_length=255)
     date_added = models.DateField(auto_now_add=True)
     price = models.IntegerField()
+    image_url = models.TextField()
     description = models.TextField()
     
